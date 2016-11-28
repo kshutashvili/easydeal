7f84a5ba6f65c921362fb2fe69df49db2aac22dd
Dmytro Kovalchuk <dmyutro@ukr.net>
2016-11-28 17:11:06 +0200

    Fix index page location

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,6 +4,5 @@ from catalog import views
 
 urlpatterns = [
     url(r'^detail/(?P<id>\d)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^$', views.main, name='main'),
     url(r'^catalog/$', views.catalog, name='catalog'),
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from catalog.forms import CatalogFilterForm
+
+
+def main(request):
+    context = {
+        'filter_form': CatalogFilterForm(request.GET),
+    }
+    return render(request, 'main.html', context)
