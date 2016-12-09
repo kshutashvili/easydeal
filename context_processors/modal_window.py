757fc0c016e5cbf3473ea7b453657c98d5f5619d
Dima Kovalchuk <dmyutro@ukr.net>
2016-12-09 17:14:52 +0200

    Add articles in modal window

--- context_processors/modal_window.py
+++ context_processors/modal_window.py
@@ -1,10 +1,15 @@
 from django.http.request import HttpRequest
 from catalog.models import Property
 from catalog.models import District
+from config.models import SiteConfiguration
 
 
+config = SiteConfiguration.get_solo()
 def modal_window(HttpRequest):
     return {
         'property_types': Property.TYPE,
-        'districts': District.objects.all()[:12]
+        'districts': District.objects.all()[:12],
+        'targets_info': config.targets_info,
+        'districts_info': config.districts_info,
+        'property_types_info': config.property_types_info,
     }
