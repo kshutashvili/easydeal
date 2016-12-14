38122b4b73637793e8affc465eae38551df27f7c
avgoeid <avgoeid@gmail.com>
2016-12-14 21:35:11 +0200

    fix catalog.url regex in detail to \d+

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from catalog import views
 
 urlpatterns = [
-    url(r'^detail/(?P<id>\d)/$', views.PropertyDetailView.as_view(), name='detail'),
+    url(r'^detail/(?P<id>\d+)/$', views.PropertyDetailView.as_view(), name='detail'),
     url(r'^catalog/$', views.catalog, name='catalog'),
 ]
